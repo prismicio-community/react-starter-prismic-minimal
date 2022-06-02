de81335c370531084c944789a98ac6149ef43d4f
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-06-01 16:02:47 -1000

    feat: add route support with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import {
-  usePrismicDocumentByUID,
-  PrismicText,
-  SliceZone,
-} from "@prismicio/react";
-
-import { components } from "../slices";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [page, { state }] = usePrismicDocumentByUID("page", "home");
-
-  if (state !== "loaded") {
-    return <div>Loading&hellip;</div>;
-  }
-
   return (
-    <div>
-      <h1>
-        <PrismicText field={page.data.title} />
-      </h1>
-      <SliceZone slices={page.data.slices} components={components} />
-    </div>
+    <main>
+      <Outlet />
+    </main>
   );
 }
 
